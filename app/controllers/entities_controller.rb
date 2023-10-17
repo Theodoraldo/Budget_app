f02bc5915658ba9db1e7b6d3c093a2e46b6ced2a
Theodoraldo <theodoraldo@gmail.com>
2023-10-17 09:01:31 +0000

    Add new entity creation and view rendering

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,28 @@
 class EntitiesController < ApplicationController
+  def new
+    @groups = Group.all
+    @entity = Entity.new
+  end
+
+  def create
+    @entity = current_user.entities.new(trans_params)
+
+    respond_to do |format|
+      if @entity.save
+        format.html { redirect_to groups_path, notice: 'Transaction created successfully.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  def show
+    @entity = current_user.entities.where(group_id: params[:id])
+  end
+
+  private
+
+  def trans_params
+    params.require(:entity).permit(:name, :amount, :group_id)
+  end
 end
